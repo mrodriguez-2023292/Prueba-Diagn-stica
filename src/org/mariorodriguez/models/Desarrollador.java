8433d9edcf20c1ba55b20b89b32dde9492312087
Informatica <informatica@alumno.local>
2024-08-30 16:08:47 -0600

    feat: se configuro correctamente la clase empleado y desarrollador

--- src/org/mariorodriguez/models/Desarrollador.java
+++ src/org/mariorodriguez/models/Desarrollador.java
@@ -1,3 +1,30 @@
-public class Desarrollador {
+package org.mariorodriguez.models;
 
+public class Desarrollador extends Empleado{
+    private String lenguajeProgramación;
+
+    public Desarrollador() {
+    }
+
+    public Desarrollador(String lenguajeProgramación, String nombre, int edad, double salario) {
+        super(nombre, edad, salario);
+        this.lenguajeProgramación = lenguajeProgramación;
+    }
+
+    public String getLenguajeProgramación() {
+        return lenguajeProgramación;
+    }
+
+    public void setLenguajeProgramación(String lenguajeProgramación) {
+        this.lenguajeProgramación = lenguajeProgramación;
+    }
+    
+    @Override
+    public void trabajar() {
+        System.out.println(nombre + " está desarrollando en " + lenguajeProgramación + ".");
+    }   
+    
+    public void trabajar(String proyecto) {
+        System.out.println(nombre + " está trabajando en el proyecto " + proyecto + " usando " + lenguajeProgramación + ".");
+    }
 }
