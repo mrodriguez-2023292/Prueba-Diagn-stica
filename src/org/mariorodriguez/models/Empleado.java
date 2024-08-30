4113f82fbd5efdb1f3dc0a705f9611470c290205
Informatica <informatica@alumno.local>
2024-08-30 15:40:54 -0600

    add: se creo la clase desarrollador en models y se agregacon metodos en la clase empleado

--- src/org/mariorodriguez/models/Empleado.java
+++ src/org/mariorodriguez/models/Empleado.java
@@ -1,5 +1,12 @@
 package org.mariorodriguez.models;
 
 public abstract class Empleado {
+    private String nombre;
+    private int edad;
+    private double salario;
     
+    public abstract void trabajar();
+    
+    public void trabajar(int horas){
+    }
 }
